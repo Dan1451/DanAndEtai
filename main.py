2b2232111e990796524de4aa46471ef4f96271a9
h <dan086421@gmail.com>
2023-08-16 14:07:40 +0300

    test

---
+++ main.py
@@ -0,0 +1,12 @@
+#1
+print(" Hello Enter your  name: Dan Hagai")
+
+#2
+print(" Hello Enter your adress: David Levontin7")
+
+
+
+"""
+name = "Name" 
+print(" Hello " , name ," How are you: " )
+"""
